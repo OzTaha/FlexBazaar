e6395aecd2d59aef47d3373f828fc1332343749b
Taha Öztürk <thoztrk007@gmail.com>
2025-01-23 01:46:03 +0300

    master remote branch silindi

--- Services/Order/Core/FlexBazaar.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
+++ Services/Order/Core/FlexBazaar.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace FlexBazaar.Order.Application.Features.CQRS.Commands.AddressCommands
 {
-    // yorum
     public class RemoveAddressCommand
     {
         public int Id { get; set; }
