25648a776751c45daf7e1b74e092d9835c494fc4
Taha Öztürk <thoztrk007@gmail.com>
2025-01-23 02:16:43 +0300

    persistance içine entity core paketleri eklendi. Veritabanı için gerekli olan ontext oluşturuldu. repositories klasörü oluşturuldu

---
+++ Services/Order/Infrastructure/FlexBazaar.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using FlexBazaar.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexBazaar.Order.Persistence.Context
+{
+    public class OrderContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("Server=...;initial Catalog=FlexBazaarOrderDb;integrated Sequrity=true;");
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+    }
+}
