f768734962d457474a34c82d4f656126fec34c63
Taha Öztürk <thoztrk007@gmail.com>
2025-01-24 02:22:07 +0300

    order detail bir kısmı eklendi.

---
+++ Services/Order/Core/FlexBazaar.Order.Application/Features/CQRS/Queries/AddressQueries/OrderDetailQueries/GetOrderDetailQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexBazaar.Order.Application.Features.CQRS.Queries.AddressQueries.OrderDetailQueries
+{
+    public class GetOrderDetailQuery
+    {
+        public int Id { get; set; }
+
+        public GetOrderDetailQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
