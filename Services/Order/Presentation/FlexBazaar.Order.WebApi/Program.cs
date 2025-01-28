a1f1dbde67cff65e7bdcb9c58eb39aafa81338e5
Taha Öztürk <thoztrk007@gmail.com>
2025-01-28 23:12:31 +0300

    Ordering class'ı için api oluşturuldu ve registiration işlemleri yapıldı.

--- Services/Order/Presentation/FlexBazaar.Order.WebApi/Program.cs
+++ Services/Order/Presentation/FlexBazaar.Order.WebApi/Program.cs
@@ -1,8 +1,15 @@
 using FlexBazaar.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using FlexBazaar.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using FlexBazaar.Order.Application.Interfaces;
+using FlexBazaar.Order.Application.Services;
+using FlexBazaar.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -14,6 +21,7 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
