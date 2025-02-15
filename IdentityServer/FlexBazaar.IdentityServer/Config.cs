7b9a9915278a93704de5b4c6e3bb020414725e2c
Taha Öztürk <thoztrk007@gmail.com>
2025-02-16 01:53:55 +0300

    config işlemlerinde visitor, manager ve admin yetkilendirmeleri yapıldı.

--- IdentityServer/FlexBazaar.IdentityServer/Config.cs
+++ IdentityServer/FlexBazaar.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -34,6 +35,46 @@ namespace FlexBazaar.IdentityServer
             new ApiScope("CatalogReadPermission", "Read authority for catalog operations"),
             new ApiScope("DiscountFullPermission", "Full authority for discount operations"),
             new ApiScope("OrderFullPermission", "Full authority for order operations")
+        };
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+
+            // visitor 
+            new Client
+            {
+                ClientId = "FlexBazaarVisitorId",
+                ClientName = "Flex Bazaar Client User",
+                // kimlik işlemleri için kullanılacak property
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("flexbazaarsecret".Sha256()) },
+                AllowedScopes = { "CatalogReadPermission"}
+            },
+
+            // Manager
+            new Client
+            {
+                ClientId = "FlexBazaarManagerId",
+                ClientName = "Flex Bazaar Manager User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("flexbazaarsecret".Sha256()) },
+                AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission"}
+            },
+
+            //Admin
+            new Client
+            {
+                ClientId = "FlexBazaarAdminId",
+                ClientName = "Flex Bazaar Admin User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("flexbazaarsecret".Sha256()) },
+                AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime = 600
+            }
         };
     }
 }
