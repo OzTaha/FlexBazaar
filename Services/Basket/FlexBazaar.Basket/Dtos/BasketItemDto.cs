fb61bce0a60b2cab7d712718edbab8d9bf2772f6
Taha Öztürk <thoztrk007@gmail.com>
2025-02-25 02:29:40 +0300

    basket mikroservisi oluşturuldu.

---
+++ Services/Basket/FlexBazaar.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FlexBazaar.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        // product id int türünde olursa uzun vadede problem yaşanabilir. Çünkü veriler catalog mikroservisten gelecek. Catalogta product id string olarak tutuluyor.
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/Basket/FlexBazaar.Basket/Dtos/BasketTotalDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FlexBazaar.Basket.Dtos
+{
+    public class BasketTotalDto
+    {
+        // kullanıcı verileri IdentityServer üzerinden gelecek. IdentityServer'da kullanıcı id string olarak tutuluyor.
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int DiscountRate { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+        public decimal TotalPrice { get => BasketItems.Sum(x => x.Price * x.Quantity); }
+    }
+}
