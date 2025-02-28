8184dbd8d8174012385a61359974648fc79ff9d6
Taha Öztürk <thoztrk007@gmail.com>
2025-02-28 19:26:11 +0300

    navbar ve carousel eklendi.

---
+++ Frontends/FlexBazaar.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexBazaar.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _CarouselDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
