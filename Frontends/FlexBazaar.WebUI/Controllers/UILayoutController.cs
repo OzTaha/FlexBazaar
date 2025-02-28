f7b8930f513b4a37dad359fc90337afbaadd5405
Taha Öztürk <thoztrk007@gmail.com>
2025-02-28 03:33:37 +0300

    frontend tarafında webUI, componentler ve layout oluşturuldu

---
+++ Frontends/FlexBazaar.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexBazaar.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        // kullanıcı layout'u
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+    }
+}
