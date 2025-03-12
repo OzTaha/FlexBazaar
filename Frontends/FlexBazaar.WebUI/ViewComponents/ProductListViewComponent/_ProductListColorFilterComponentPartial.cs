0bb790f8bf7b0cad6b8a05095294f7610717791b
Taha Öztürk <thoztrk007@gmail.com>
2025-03-12 18:17:59 +0300

    ProductList tamamlandı

---
+++ Frontends/FlexBazaar.WebUI/ViewComponents/ProductListViewComponent/_ProductListColorFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexBazaar.WebUI.ViewComponents.ProductListViewComponent
+{
+    public class _ProductListColorFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
