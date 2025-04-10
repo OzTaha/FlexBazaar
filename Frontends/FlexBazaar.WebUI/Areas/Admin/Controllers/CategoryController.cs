935c018fa13d4c2fbfa2e01a068023a8ffe9ce69
Taha Öztürk <thoztrk007@gmail.com>
2025-04-10 05:45:30 +0300

    Admin kategori güncelleme işlemleri yapıldı.

--- Frontends/FlexBazaar.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/FlexBazaar.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -86,5 +86,44 @@ namespace FlexBazaar.WebUI.Areas.Admin.Controllers
             }
             return View();
         }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+
+            ViewBag.v1 = "Anasaya";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Güncelleme Sayfası";
+            ViewBag.v0 = "Kategori İşlemleri";
+      
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7017/api/Categories/" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            // metin formatındaki değeri alıp json formatına çeviriyor (serialize)
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:7017/api/Categories/", stringContent );
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
     }
 }
