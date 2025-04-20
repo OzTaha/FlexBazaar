bf0678902f2f186e48ad4acfcbd7145d77a04ba6
Taha Öztürk <thoztrk007@gmail.com>
2025-04-21 00:57:38 +0300

    Anasayfa "Öne Çıkan Alan" UI tarafında gösterimi tamamlandı.

--- Frontends/FlexBazaar.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
+++ Frontends/FlexBazaar.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FlexBazaar.DtoLayer.CatalogDtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace FlexBazaar.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("http://localhost:7017/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
