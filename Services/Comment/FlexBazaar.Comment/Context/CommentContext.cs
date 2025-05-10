e6764e06edfe426fa187231881c2a61bafb6b442
Taha Öztürk <thoztrk007@gmail.com>
2025-05-10 04:01:57 +0300

    Yorumlar servisi oluşturuldu.

---
+++ Services/Comment/FlexBazaar.Comment/Context/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using FlexBazaar.Comment.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexBazaar.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442;initial Catalog=FlexBazaarCommentDb;User=sa;Password=Qwe1-asd23..");
+        }
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
