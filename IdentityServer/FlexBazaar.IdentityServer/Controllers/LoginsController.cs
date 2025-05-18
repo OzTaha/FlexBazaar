27b07ccd292545db56b216acf06a3731bb8614ae
Taha Öztürk <thoztrk007@gmail.com>
2025-05-18 04:12:29 +0300

    login api oluşturuldu. UI kısmı için login sayfası hazırlandı. Kullanıcının giriş yapabilmesi için gerekli olan token işlemlerine başlandı.

---
+++ IdentityServer/FlexBazaar.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,35 @@
+﻿using FlexBazaar.IdentityServer.Dtos;
+using FlexBazaar.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FlexBazaar.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+         private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Ok("Giriş Başarılı");
+            }
+            else
+            {
+                return BadRequest("Kullanıcı Adı veya Şifre Hatalı");
+            }
+        }
+    }
+}
