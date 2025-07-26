1c079e811feebf7c0531859254cb722d8861b84e
Taha Öztürk <thoztrk007@gmail.com>
2025-07-26 04:49:51 +0300

    Kredi Kartı Ödeme Sayfası kredi kartı ödeme sayfası oluşturuldu ve tasarımda düzenlemeler yapıldı.

---
+++ Frontends/FlexBazaar.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexBazaar.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.breadcrumb1 = "Anasayfa";
+            ViewBag.breadcrumb2 = "Ödeme Ekranı";
+            ViewBag.breadcrumb3 = "Kartla Ödeme";
+
+            return View();
+        }
+    }
+}
