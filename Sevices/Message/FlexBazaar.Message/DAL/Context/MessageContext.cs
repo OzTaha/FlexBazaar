275fd677b0dd88606c76a7e9be8ddf1e962d001e
Taha Öztürk <thoztrk007@gmail.com>
2025-07-30 02:49:01 +0300

    Message Mikroservisi Mesaj mikroservisi kuruldu context ve entity oluşturuldu entity framework ve npgsql paketleri kuruldu ve postgreSQL ile bağlantı sağlandı.

---
+++ Sevices/Message/FlexBazaar.Message/DAL/Context/MessageContext.cs
@@ -0,0 +1,14 @@
+﻿using FlexBazaar.Message.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexBazaar.Message.DAL.Context
+{
+    public class MessageContext :DbContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<UserMessage> UserMessages { get; set; }
+    }
+}
