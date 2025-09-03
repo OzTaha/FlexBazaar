bc8c128ed3cf6325641917ad0bafbfb1bc5947bf
Taha Öztürk <thoztrk007@gmail.com>
2025-09-03 20:55:35 +0300

    rabbitMQ mesaj okuma işlemi tamamlandı.

--- Services/RabbitMQMessage/FlexBazaar.RabbitMQMessageApi/Controllers/MessageController.cs
+++ Services/RabbitMQMessage/FlexBazaar.RabbitMQMessageApi/Controllers/MessageController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,15 +22,51 @@ namespace FlexBazaar.RabbitMQMessageApi.Controllers
 
             var channel = await connection.CreateChannelAsync();
 
-            await channel.QueueDeclareAsync("Kuyruk1", false, false, false, arguments: null);
+            await channel.QueueDeclareAsync("Kuyruk2", false, false, false, arguments: null);
 
-            var messageContent = "Merhaba bu bir RabbitMQ kuyruk mesajıdır.";
+            var messageContent = "Merhaba bugün hava çok soğuk.";
 
             var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
 
-            await channel.BasicPublishAsync(exchange:"",routingKey:"Kuyruk1",body: byteMessageContent);
+            await channel.BasicPublishAsync(exchange:"",routingKey: "Kuyruk2", body: byteMessageContent);
 
             return Ok("Mesajınız Kuyruğa Alınmıştır");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ReadMessage()
+        {
+            var factory = new ConnectionFactory();
+
+            factory.HostName = "localhost";
+
+            var connection = await factory.CreateConnectionAsync();            
+
+            using(var channel = await connection.CreateChannelAsync())
+            {
+              await channel.QueueDeclareAsync("Kuyruk2", false, false, false, arguments: null);
+
+                BasicGetResult? result = await channel.BasicGetAsync(queue: "Kuyruk2", autoAck: false);
+
+                if (result is null)
+                {
+                    return Ok("Kuyrukta okunacak mesaj bulunamadı.");
+                }
+
+                try
+                {
+                    var messageBody = result.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(messageBody);
+
+                   await channel.BasicAckAsync(deliveryTag: result.DeliveryTag, multiple: false);
+
+                    return Ok(message);
+                }
+                catch(Exception)
+                {
+                    return StatusCode(500, "Mesaj işlenirken bir hata oluştu.");
+                }
+            }                        
+        }      
     }
 }
