9382585bb8baf68360ac6ca5fc0ea63d2a5f9ad0
Taha Öztürk <thoztrk007@gmail.com>
2025-09-14 03:25:51 +0300

    Rapid API hava durumu bilgisi ve Döviz kuru bilgisi eklendi.

---
+++ RapidApi/FlexBazaar.RapidApiWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,80 @@
+﻿using FlexBazaar.RapidApiWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FlexBazaar.RapidApiWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> WeatherDetail()
+        {
+            // client amacı API projelerinde HttpPut, HttpGet gibi metodları çağırır
+            var client = new HttpClient();
+            // request amacı API projelerinde istek yapılacak olan adresi ve o adresin parametrelerini girmek 
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/Antalya"),
+                Headers =
+    {
+        { "x-rapidapi-key", "6016f221dcmshe54fa948372cdd6p1a4198jsn3cc0f2cff5d1" },
+        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<WeatherViewModel.Rootobject>(body);
+                ViewBag.cityTemp = values.data.temp;
+                return View(values);
+            }
+        }
+
+        public async Task<IActionResult> Exchange()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "6016f221dcmshe54fa948372cdd6p1a4198jsn3cc0f2cff5d1" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.exchangeRateUsd = values.data.exchange_rate;
+                ViewBag.previousCloseUsd = values.data.previous_close;
+            }
+
+
+
+            var clientEur = new HttpClient();
+            var requestEur = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=EUR&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "6016f221dcmshe54fa948372cdd6p1a4198jsn3cc0f2cff5d1" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await clientEur.SendAsync(requestEur))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.exchangeRateEur = values.data.exchange_rate;
+                ViewBag.previousCloseEur = values.data.previous_close;
+                return View();
+            }
+        }
+    }
+}
